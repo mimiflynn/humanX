a8c26611f4ea47ca8c69d62e15eec8734902e7f2
Mimi Flynn <mimiflynn@gmail.com>
2013-01-13 15:31:19 -0500

    homepage merge conflict resolution attempt

--- wp-content/themes/humanX/section-home.php
+++ wp-content/themes/humanX/section-home.php
@@ -10,7 +10,6 @@
                 <p class="description">Choose From Our List of Top Rated Products</p>
               </header>
               <div class="main-content">
-<<<<<<< HEAD
                 
                 <div class="row">
                 
@@ -30,42 +29,7 @@
                   ?>
                  
                 <div><!-- .row --> 
-=======
 
-
-
-              <div class="row">
-                <div class="span6">
-                  <div class="product-category">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/deodorant.jpg"></a>
-                    <h2><a href="#">Deodorant</a></h2>
-                  </div>
-                  <div class="product-category">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/shampoo.jpg"></a>
-                    <h2><a href="#">Shampoo</a></h2>
-                  </div>
-                  <div class="product-category">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/shaving-cream.jpg"></a>
-                    <h2><a href="#">Shaving Cream</a></h2>
-                  </div>
-                </div><!-- .span --> 
-              <div><!-- .row --> 
-
-
-              <div class="row">
-                <div class="span6">
-                  <div class="product-category">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/cleaner.jpg"></a>
-                    <h2><a href="#">Multipurpose Cleaner</a></h2>
-                  </div>
-                  <div class="product-category">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/detergent.jpg"></a>
-                    <h2><a href="#">Detergent</a></h2>
-                  </div>
-                </div><!-- .span --> 
-              <div><!-- .row --> 
-
->>>>>>> b7332894982ce50979973f7377d0605cde2eabcc
 
               </div>
             </article>

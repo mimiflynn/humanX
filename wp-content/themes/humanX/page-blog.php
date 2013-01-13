3a4eaa6db6d2ed261904d0db70edd5af93d1994c
Mimi Flynn <mimiflynn@gmail.com>
2013-01-13 13:24:28 -0500

    move quiz to page template and remove from index attempt 2

---
+++ wp-content/themes/humanX/page-blog.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+      <div id="body" class="content">
+        <div id="main">
+        <?php get_template_part('section', 'blog'); ?>
+        </div> 
+      </div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/humanX/page-quiz.php
@@ -0,0 +1,11 @@
+<?php 
+  /*
+    Template Name: Membership Landing Page
+  */
+  get_header(); ?>
+      <div id="body" class="content">
+        <div id="main">
+        <?php get_template_part('section', 'quiz'); ?>
+        </div> 
+      </div>
+<?php get_footer(); ?>

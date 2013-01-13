841b7afec4848379ddb78f5ed895e6581e0df1ad
Mimi Flynn <mimiflynn@gmail.com>
2013-01-13 11:18:47 -0500

    product field output

--- wp-content/themes/humanX/content-product.php
+++ wp-content/themes/humanX/content-product.php
@@ -11,8 +11,21 @@
 		</header><!-- .entry-header -->
 		<div class="entry-summary">
 			<?php the_excerpt(); ?>
-			Price: $<?php the_field('price'); ?>
-			Rating: <?php the_field('rating'); ?>
-			<a href="<?php the_field('purchase_url'); ?>">Buy Online</a>
+			<?
+			$acf_image = get_field('image');
+      if ($acf_image) { ?>
+			<div class="field"><img src="<?php the_field('image'); ?>"></div>
+        <?php
+      }
+			?>
+			<div class="field">Price: $<?php the_field('price'); ?></div>
+			<div class="field">Rating: <?php the_field('rating'); ?></div>
+			<?
+      $acf_url = get_field('purchase_url');
+      if ($acf_url) { ?>
+			<div class="field"><a href="<?php the_field('purchase_url'); ?>">Buy Online</a></div>
+			  <?php
+      }
+      ?>
 		</div><!-- .entry-summary -->
   </article><!-- #post-<?php the_ID(); ?> -->

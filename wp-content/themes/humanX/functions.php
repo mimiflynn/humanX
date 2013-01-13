720aabebd4491804a33da2b8fc63f54bafca00a9
castig <castig@gmail.com>
2013-01-13 12:19:56 -0500

    add bootstrap CSS to header.php, changed .wrapper to .container

--- wp-content/themes/humanX/functions.php
+++ wp-content/themes/humanX/functions.php
@@ -185,6 +185,35 @@ function create_post_type() {
 
 
 
+// move admin bar to bottom
+function fb_move_admin_bar() { ?>
+	<style type="text/css">
+		body {
+			margin-top: -28px;
+			padding-bottom: 28px;
+		}
+		body.admin-bar #wphead {
+			padding-top: 0;
+		}
+		body.admin-bar #footer {
+			padding-bottom: 28px;
+		}
+		#wpadminbar {
+			top: auto !important;
+			bottom: 0;
+		}
+		#wpadminbar .quicklinks .menupop ul {
+			bottom: 28px;
+		}
+	</style>
+<?php }
+// on backend area
+add_action( 'admin_head', 'fb_move_admin_bar' );
+// on frontend area
+add_action( 'wp_head', 'fb_move_admin_bar' );
+
+
+
 
 
 ?>
--- wp-content/themes/humanX/header.php
+++ wp-content/themes/humanX/header.php
@@ -42,6 +42,9 @@
         wp_enqueue_script( 'comment-reply' );
       wp_head();
     ?>
+     <link href="<?php echo get_template_directory_uri(); ?>/stylesheets/bootstrap/bootstrap.css" rel="stylesheet" type="text/css" />
+    <!-- <link href="<?php echo get_template_directory_uri(); ?>/stylesheets/bootstrap/bootstrap-responsive.css" rel="stylesheet" type="text/css" /> -->
+   
     <link href="<?php echo get_template_directory_uri(); ?>/stylesheets/animate-custom.css" media="screen, projection" rel="stylesheet" type="text/css" />
     <link href="<?php echo get_template_directory_uri(); ?>/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
     <link href="<?php echo get_template_directory_uri(); ?>/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
@@ -56,11 +59,47 @@
   </head>
 
   <body <?php body_class(); ?>>
+     <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">The Human Experiment</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+
+
+
   <!-- Prompt IE 6 users to install Chrome Frame..
        chromium.org/developers/how-tos/chrome-frame-getting-started -->
   <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
   <a href="http://localhost/humanX/wp-login.php?loginFacebook=1&redirect=http://localhost/humanX" onclick="window.location = 'http://localhost/humanX/wp-login.php?loginFacebook=1&redirect='+window.location.href; return false;">Click here to login or register with Facebook</a>
-  <div class="wrapper"><div class="inner">
+  <div class="container"><div class="inner">
     <header id="branding" role="banner">
       <h1 id="site-title"><a href="<?php bloginfo( 'siteurl' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
     </header><!-- #branding -->
